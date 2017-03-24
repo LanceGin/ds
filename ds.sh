e0d681914ba607f7cb46268152ae54ebac8d313e
lancegin <gin.lance.inside@hotmail.com>
2017-03-24 11:08:04 +0800

    add: install jq script

--- ds.sh
+++ ds.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # get the words needs to be translated
-unkmown_word="$1";
+unkmown_word="$1"
 
 # get the YouDao api token
-keyfrom="gin-shell";
-key="1361115375";
+keyfrom="gin-shell"
+key="1361115375"
 
 # define a function to open a HTTP request
 function translate()
 {
-  base_url="http://fanyi.youdao.com/openapi.do";
-  req_params="keyfrom=$keyfrom&key=$key&type=data&doctype=json&version=1.1&only=dict&q=$1";
-  res=`curl -s -H "charset=UTF-8" -d $req_params "http://fanyi.youdao.com/openapi.do"`;
-  translate_res=$res;
-  return;
+  base_url="http://fanyi.youdao.com/openapi.do"
+  req_params="keyfrom=$keyfrom&key=$key&type=data&doctype=json&version=1.1&only=dict&q=$1"
+  res=`curl -s -H "charset=UTF-8" -d $req_params "http://fanyi.youdao.com/openapi.do"`
+  translate_res=$res
+  return
 }
 
 # get the translated response
-translate $unkmown_word;
+translate $unkmown_word
 
 # use jq to parse the response json
-phonetic=`echo $translate_res | jq '.basic.phonetic'`;
-explains=`echo $translate_res | jq '.basic.explains[]'`;
+phonetic=`echo $translate_res | jq '.basic.phonetic'`
+explains=`echo $translate_res | jq '.basic.explains[]'`
 
 # print the data
-echo "$unkmown_word[$phonetic]";
-echo "翻译：";
-echo "$explains";
+echo "$unkmown_word[$phonetic]"
+echo "翻译："
+echo "$explains"
--- install.sh
+++ install.sh
@@ -13,48 +13,50 @@ echo "****"
 
 # check whether exists $DS_KEYFROM and $DS_KEY
 if [ ! $DS_KEYFROM ]; then
-  echo "Use the default api key or set by yourself? [D/S] D -> defalut, S -> Set";
+  echo "Use the default api key or set by yourself? [D/S] D -> defalut, S -> Set"
   read is_default
 
   # check the value get from user
   while [ "$is_default" != "D" ] && [ "$is_default" != "S" ] && [ "$is_default" != "d" ] && [ "$is_default" != "s" ]; do
-    echo "Use the default api key or set by yourself? [D/S] D -> defalut, S -> Set";
+    echo "Use the default api key or set by yourself? [D/S] D -> defalut, S -> Set"
     read is_default
   done
 
   # default key or reset?
   if [ $is_default = 'D' ] || [ $is_default = 'd' ]; then
-    DS_KEYFROM="gin-shell";
-    export DS_KEYFROM;
-    DS_KEY="1361115375";
-    export DS_KEY;
-    echo "DS_KEYFROM: $DS_KEYFROM";
-    echo "DS_KEY: $DS_KEY";
+    DS_KEYFROM="gin-shell"
+    export DS_KEYFROM
+    DS_KEY="1361115375"
+    export DS_KEY
+    echo "DS_KEYFROM: $DS_KEYFROM"
+    echo "DS_KEY: $DS_KEY"
   elif [ $is_default = 'S' ] || [ $is_default = 's' ]; then
-    echo "reset key";
+    echo "reset key"
   fi
 fi
 
-# define a function to install ds bin shell
-function install_ds_bin()
-{
-  echo "install ds bin shell";
-}
-
-# define a function to install jq if necessary
-function install_jq()
-{
-  echo "install jq";
-}
-
 # check whether exists jq
-if [ hash jq 2>/dev/null ]; then 
-  install_ds_bin;
-else 
-  install_jq;
-  install_ds_bin;
-fi
+hash jq >/dev/null 2>&1 || {
+  echo "****"
+  echo "**** jq not installed."
+  echo "**** start installing jq"
+  echo "****"
+  git clone --depth=1 https://github.com/stedolan/jq.git /opt || {
+    echo "Error: git clone jq repo failed\n"
+    exit 1
+  }
+  cd /opt/jq
+  autoreconf -i
+  ./configure --disable-maintainer-mode
+  make
+  sudo make install
+}
 
+# define a function to install ds bin shell
+echo "install ds bin shell"
+
+
+# success
 echo '         __             '
 echo '     ___/ /        _____'
 echo '   /`__  /       / ___/ '
