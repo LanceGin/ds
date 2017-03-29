22c6fba064bf4db2518b0dfb49646055b5d84ef2
lancegin <gin.lance.inside@hotmail.com>
2017-03-29 10:48:00 +0800

    fix: transfer $

--- bin/command.sh
+++ bin/command.sh
@@ -20,8 +20,8 @@ uninstall_ds()
 {
   rm -rf ~/.ds
   rm /usr/local/bin/ds
-  unset $DS_KEY
-  unset $DS_KEYFROM
+  unset \$DS_KEY
+  unset \$DS_KEYFROM
   sed -i '/DS_KEY/d' ~/.bashrc
   sed -i '/DS_KEYFROM/d' ~/.bashrc
   # success
@@ -30,7 +30,7 @@ uninstall_ds()
   echo '****   /`__  /       / ___/ '
   echo '****  / /_/ /  __   (__  )  '
   echo '**** /_ _ _/  /_/ /_____/   '
-  echo '****                           ....is now updated!'
+  echo '****                           ....is now uninstalled!'
   echo '****'
   echo '**** nothing would be remained in the system.'
   echo '****'
